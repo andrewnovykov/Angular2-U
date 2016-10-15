f4d1198bbe8c51c6338fec82cbc890437bc75658
papapin <papapin777@gmail.com>
2016-10-15 16:40:30 +0300

    Manipulating  template2

--- src/app/my-component.component.ts
+++ src/app/my-component.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-component',
-	template: `HI im {{name}}! <span [class.awesome]="inputElement.value === 'yes'">It is awesome!</span>
+	template: `HI im <span [style.color]="inputElement.value === 'yes' ? 'red' : '' ">{{name}}</span>!
+		<span [class.awesome]="inputElement.value === 'yes'">It is awesome!</span>
 		<br />
 		<br />
 		<input type="text" #inputElement (keyup)="0" />
+		<br />
+		<br />
+		<button [disabled]="inputElement.value !== 'yes'" >
+			Only enabled if 'yes' was entered
+		</button>
+
 
 	`,
 	styleUrls: ['./my-component.component.css']
