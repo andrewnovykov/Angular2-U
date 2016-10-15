a8a095a1c985aaa94f04ef890400d6f2357a6994
papapin <papapin777@gmail.com>
2016-10-15 18:08:47 +0300

    First puzzle app

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuzzleComponent implements OnInit {
 
-  constructor() { }
+	switch1Number: number;
+	switch2Number: number;
+	switch3Number: number;
+	switch4Number: number;
+
+  // constructor(switch1Number,switch2Number,switch3Number,switch4Number) { 
+  // 	this.switch1Number = switch1Number;
+  // 	this.switch2Number = switch2Number;
+  // 	this.switch3Number = switch3Number;
+  // 	this.switch4Number = switch4Number;
+  // }
 
   ngOnInit() {
+
+  	this.switch1Number = Math.round(Math.random());
+  	this.switch2Number = Math.round(Math.random());
+  	this.switch3Number = Math.round(Math.random());
+  	this.switch4Number = Math.round(Math.random());
+
+  	console.log(this.switch1Number, this.switch2Number, this.switch3Number, this.switch4Number)
+
   }
+
+
 
 }
